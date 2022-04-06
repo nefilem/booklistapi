2efadda0d8a37ae7f11deb9d11e64d6a6b5d98cc
Keiron Goodwin <keirong@gmail.com>
2022-04-06 11:25:01 +0100

    Modified index.js to add heroku friendly code

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+//const port = 3000;
+const port = process.env.PORT || 3000
 const mongoose = require('mongoose');
 const uri = "mongodb+srv://dbMongo:pa55word@cluster0.trvjy.mongodb.net/booklist?retryWrites=true&w=majority";
 
