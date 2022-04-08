fe6ab41a7304ca75f1a9a4a057b374e72fe5c971
Lana <80698898+Lana2025@users.noreply.github.com>
2022-04-08 10:17:37 +0100

    Add home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import BooksList from './BooksList'
 import BooksInsert from './BooksInsert'
 import BooksUpdate from './BooksUpdate'
+import Home from './Home'
 
-export { BooksList, BooksInsert, BooksUpdate }
+export { BooksList, BooksInsert, BooksUpdate, Home }
+
